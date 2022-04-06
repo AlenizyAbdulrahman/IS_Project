68533ef2def2cd222b41a434987069b5b32c466e
abdulrahman-alanzi <ksa_501@outlook.sa>
2022-04-06 03:23:12 +0300

    create keyes

--- login.py
+++ login.py
@@ -9,6 +9,52 @@ from tkinter import messagebox
 from nbformat import write
 import hashlib
 import random 
+import rsa
+
+
+class mainpage(Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("650x600")
+        self.title('login')
+        self.config(bg='#669BBC')
+        self.resizable(False,False)
+
+        frame = Frame(self,bg='#669BBC')
+        frame.place(x=120,y=50,width=500,height=400)
+
+        headline = Label(frame, text='Please select option.', fg='white',bg='#669BBC',font=('Courier',30,'bold'),pady=0).place(x=0)
+        btn_send = Button(frame,text='Send file',bg='#F3A712',bd=0,font=('Courier',18),command=self.send).place(x=50,y=200,width=350,height=50)
+        btn_recive = Button(frame,text='Recive file',bg='#F3A712',bd=0,font=('Courier',18),command=self.recieve).place(x=50,y=300,width=350,height=50)
+    def send(self):
+        sender()
+        self.destroy()
+    def recieve(self):
+        reciever()
+        self.destroy()
+
+
+class sender(Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("650x600")
+        self.title('login')
+        self.config(bg='#669BBC')
+        self.resizable(False,False)
+
+        frame = Frame(self,bg='#669BBC')
+        frame.place(x=120,y=50,width=500,height=400)
+
+class reciever(Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("650x600")
+        self.title('login')
+        self.config(bg='#669BBC')
+        self.resizable(False,False)
+
+        frame = Frame(self,bg='#669BBC')
+        frame.place(x=120,y=50,width=500,height=400)
 
 
 class signinpage(Tk):
@@ -19,54 +65,64 @@ class signinpage(Tk):
         self.config(bg='#669BBC')
         self.resizable(False,False)
 
-        frame2 = Frame(self,bg='#669BBC')
-        frame2.place(x=120,y=50,width=550,height=500)
+        self.frame2 = Frame(self,bg='#669BBC')
+        self.frame2.place(x=120,y=50,width=550,height=500)
 
-        headline2 = Label(frame2, text='Sign-in', fg='white',bg='#669BBC',font=('Courier',55,'bold'),pady=20).place(x=100)
+        self.headline2 = Label(self.frame2, text='Sign-in', fg='white',bg='#669BBC',font=('Courier',55,'bold'),pady=20).place(x=100)
 
-        txt_email2 = self.labels2(frame2,"Email",120)
-        self.email2 = self.entrys2(frame2,185)
+        self.txt_email2 = Label(self.frame2,text='Username',fg='white',bg='#669BBC',font=('Courier',18),pady=20).place(x=30,y=120)
+        self.email2 = Entry(self.frame2,font=('Courier',18,'bold'))
+        self.email2.place(x=30,y=185,width=350,height=45)
 
-        txt_pass2 = self.labels2(frame2, 'Password',240)
-        self.password2 = self.entrys2(frame2,300)
+        self.txt_pass2 = Label(self.frame2,text='Password',fg='white',bg='#669BBC',font=('Courier',18),pady=20).place(x=30,y=240)
+        self.password2 = Entry(self.frame2,font=('Courier',18,'bold'))
+        self.password2.place(x=30,y=300,width=350,height=45)
 
-        
+        self.btn_login = Button(self.frame2,text='Register',bg='#F3A712',bd=0,font=('Courier',18),command=self.register).place(x=30,y=380,width=350,height=50)
 
-        btn_login = Button(frame2,text='Register',bg='#F3A712',bd=0,font=('Courier',18),command=self.register).place(x=30,y=380,width=350,height=50)
-
-    def labels2(self,pos,text,y):
-        Label(pos,text=text,fg='white',bg='#669BBC',font=('Courier',18),pady=20).place(x=30,y=y)
-
-    def entrys2(self,pos,y):
-        entry_obj = Entry(pos,font=('Courier',18,'bold'))
-        entry_obj.place(x=30,y=y,width=350,height=45)
-        return entry_obj 
 
     def register(self):
-        self.ran = random.randint(0,100)
-        email_in = str(self.email2.get())
-        pass_in = str(self.password2.get()+str(self.ran))
 
-        hash_obj = hashlib.md5(pass_in.encode())
-        md5_hash = hash_obj.hexdigest()
+        self.ran = random.randint(0,100)
+        self.email_in = str(self.email2.get())
+        self.pass_in = str(self.password2.get()+str(self.ran))
+
+        self.hash_obj = hashlib.md5(self.pass_in.encode())
+        self.md5_hash = self.hash_obj.hexdigest()
 
     # python object to be appended
-        y = [ {  "password": md5_hash, "salt": self.ran  } ]
-        write_json(y,str(email_in))
+        self.data = [ {  "password": self.md5_hash, "salt": self.ran  } ]
+        self.write_json(self.data,str(self.email_in),'users_data.json')
+        self.generate_keys()
         self.destroy()
         login()
 
-# function to add to JSON
-def write_json(new_data,d, filename='users_data.json'):
-    with open(filename,'r+') as file:
-          # First we load existing data into a dict.
-        file_data = json.load(file)
-        # Join new_data with file_data inside emp_details
-        file_data[d]=(new_data)
-        # Sets file's current position at offset.
-        file.seek(0)
-        # convert back to json.
-        json.dump(file_data, file, indent = 4)
+    # function to add to JSON
+    def write_json(self,new_data,email, filename):
+        with open(filename,'r+') as file:
+            # First we load existing data into a dict.
+            file_data = json.load(file)
+            # Join new_data with file_data inside emp_details
+            file_data[email]=(new_data)
+            # Sets file's current position at offset.
+            file.seek(0)
+            # convert back to json.
+            json.dump(file_data, file, indent = 4)
+    
+    def generate_keys(self):
+        #create pri and pub keys
+        (self.publickey,self.privatekey) = rsa.newkeys(1025)
+
+        #write pub key in file
+        self.pubkey = open(f"{self.email_in}PublicKey.key",'wb')
+        self.pubkey.write(self.publickey.save_pkcs1('PEM'))
+        self.pubkey.close()
+
+        #write pri key in file
+        self.prikey = open(f"{self.email_in}PrivateKey.key",'wb')
+        self.prikey.write(self.privatekey.save_pkcs1('PEM'))
+        self.prikey.close()
+        
 
 
     
@@ -83,29 +139,22 @@ class login(Tk):
 
         headline = Label(frame, text='Login', fg='white',bg='#669BBC',font=('Courier',55,'bold'),pady=20).place(x=100)
 
-        txt_email = self.labels(frame,"Email",120)
-        self.email = self.entrys(frame,185)
+        txt_email = Label(frame,text="Username",fg='white',bg='#669BBC',font=('Courier',18),pady=20).place(x=30,y=120)
+        self.email = Entry(frame,font=('Courier',18,'bold'))
+        self.email.place(x=30,y=185,width=350,height=45)
 
-        txt_pass = self.labels(frame, 'Password',240)
-        self.password = self.entrys(frame,300)
+        txt_pass = Label(frame,text='Password',fg='white',bg='#669BBC',font=('Courier',18),pady=20).place(x=30,y=240)
+        self.password = Entry(frame,font=('Courier',18,'bold'))
+        self.password.place(x=30,y=300,width=350,height=45)
 
         btn_login = Button(frame,text='Login',bg='#F3A712',bd=0,font=('Courier',18),command=self.login).place(x=30,y=380,width=350,height=50)
         btn_signin = Button(frame,text='Sign-in',bg='#F3A712',bd=0,font=('Courier',10),command=self.signin).place(x=160,y=450,width=100,height=40)
-        
-
-    def labels(self,pos,text,y):
-        Label(pos,text=text,fg='white',bg='#669BBC',font=('Courier',18),pady=20).place(x=30,y=y)
-
-    def entrys(self,pos,y):
-        entry_obj = Entry(pos,font=('Courier',18,'bold'))
-        entry_obj.place(x=30,y=y,width=350,height=45)
-        return entry_obj 
 
     def signin(self):
         signinpage()
         self.destroy()
 
-
+    # this function will check if the user registered or not if it is register it will login into home page
     def login(self):
         record1 = json.load(open("users_data.json"))          
         pass_list = [ d["password"] for d in record1[str(self.email.get())] ]
@@ -117,7 +166,7 @@ class login(Tk):
 
             if (md5_hash) in pass_list:
                 self.destroy()
-                root = Tk()
+                mainpage()
                 
             else:
                 messagebox.showerror("Error", "Incorrect Email or Password, Please Try Again.")
